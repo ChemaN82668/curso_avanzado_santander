243cdb604762973329af4174a2f84e9d1273e876
ChemaN82668 <josmhurtado@gruposantander.com>
2024-09-16 10:30:53 +0200

    Update csv_json.py

--- codigo_sep_16_20/csv_json.py
+++ codigo_sep_16_20/csv_json.py
@@ -4,16 +4,8 @@ Convertir formato CSV a JSon
 
 path = "../ficheros_curso/merge/Empleados.txt"
 
-def csvJson(csv, sep=";"):
-    L = csv.split("\n")
-    cabs = L[0]
-    resul = []
-    for i in L[1:]:
-        if len(i) > 0:
-            d = dict(zip(cabs.split(sep), i.split(sep)))
-            resul.append(d)
-    return resul
-
+def csvJson(csv):
+    print(csv)
 
 def leerFichero():
     fich = open(path, "r")
@@ -21,17 +13,6 @@ def leerFichero():
     fich.close()
     return csv
 
-def jsoncsv(resul, sep=";"):
-    cabs = sep.join(resul[0].keys())
-    L = [cabs]
-    for d in resul:
-        linea = sep.join(d.values())
-        L.append(linea)
-    return "\n".join(L)
-
 if __name__=='__main__':
     csv = leerFichero()
-    resul = csvJson(csv)
-    print(resul)
-    resul2 = jsoncsv(resul)
-    print(resul2)
+    csvJson(csv)
